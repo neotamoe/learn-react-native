d698ce3fb2c352d389cd4cbadf7b5534a56ef190
neotamoe <neota.moe@gmail.com>
2019-02-06 14:03:31 -0600

    change key to be string, using react-native-debugger, changes to configureStore

--- AwesomeProjectSetup/src/store/reducers/places.js
+++ AwesomeProjectSetup/src/store/reducers/places.js
@@ -11,7 +11,7 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 places: state.places.concat({
-                    key: Math.random(), 
+                    key: Math.random().toString(), 
                     name: action.placeName,
                     image: {
                         uri: "https://cdn.pixabay.com/photo/2016/07/06/03/48/minneapolis-1499809_960_720.jpg"

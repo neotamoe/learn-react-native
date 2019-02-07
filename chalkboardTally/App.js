74d1b50af9693d3d69142ba0418bdf691da18701
neotamoe <neota.moe@gmail.com>
2019-02-07 11:44:02 -0600

    multiple user displays in a scrollview; need to refactor state

--- chalkboardTally/App.js
+++ chalkboardTally/App.js
@@ -8,60 +8,21 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, ImageBackground, FlatList} from 'react-native';
+import {StyleSheet, View, ImageBackground, ScrollView} from 'react-native';
 import chalkboard from './src/chalkboard.jpg';
-
-import moment from 'moment';
+import Ben from './src/components/ben';
+import Neota from './src/components/neota';
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    startDate: moment("2018-08-07", "YYYY-MM-DD"),
-    totalDays: 0,
-    currentDate: moment(),
-    dayCount: -1,
-    days: [],
-    isTallyComplete: false
-  }
-
-  componentDidMount(){
-    const today = moment(this.state.currentDate);
-    const start = moment(this.state.startDate);
-    const calculatedDays = today.diff(start, 'days');
-    this.setState({ totalDays: calculatedDays});
-    this.interval = setInterval(() => this.tick(), 500);
-  }
-
-  tick() {
-    if(this.state.dayCount + 1 < this.state.totalDays){
-      this.setState(prevState => ({
-        dayCount: prevState.dayCount + 1,
-        days: prevState.days.concat({ key: (prevState.dayCount + 1).toString() })
-      }));  
-    } else {
-      clearInterval(this.interval);
-      this.setState({isTallyComplete: true})
-    }
-  }
-
   render() {
-    let summary = this.state.isTallyComplete ? <View style={styles.container}>
-          {/* <Text style={{color: 'white', margin: 5}}>Start: {this.state.startDate}</Text>
-          <Text style={{color: 'white', margin: 5}}>Today: {this.state.currentDate}</Text> */}
-          <Text style={{color: 'white', margin: 5, fontSize: 40, fontFamily: 'ChalkboardSE-Bold'}}>{this.state.totalDays} Days</Text>
-        </View> : null;
     return (
       <View >
         <ImageBackground source={chalkboard} style={styles.chalkboard}>
-            <Text style={{color: 'white', fontSize: 40, fontFamily: 'ChalkboardSE-Bold'}}>Tron Tally</Text>
-            <FlatList
-              data={this.state.days}
-              style={styles.list}
-              horizontal={false}
-              numColumns={35}
-              renderItem={({item}) => <Text style={ (parseInt(item.key)+1)%5==0 ? styles.tallyRotate : styles.tally}></Text>}
-            />
-            {summary}
+          <ScrollView>
+            <Ben />
+            <Neota />
+          </ScrollView>
         </ImageBackground>
       </View>
     );
@@ -69,45 +30,10 @@ export default class App extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   chalkboard: {
     width: "100%",
     height: "100%",
     alignItems: "center",
     paddingTop: 40,
   },
-  text: {
-    fontSize: 16,
-    textAlign: "center",
-  },
-  list: {
-    flexDirection: "column",
-    marginTop: 25,
-  },
-  tally: {
-    display: "flex",
-    height: 30,
-    width: 1,
-    borderWidth: 1,
-    borderColor: "lightgrey",
-    borderStyle: "solid",
-    marginRight: 4,
-    marginTop: 5,
-  },
-  tallyRotate: {
-    display: "flex",
-    height: 40,
-    width: 1,
-    borderWidth: 1,
-    borderColor: "lightgrey",
-    borderStyle: "solid",
-    marginRight: 4,
-    marginTop: 0,
-    left: -15,
-    transform: [{rotate: '320deg'}],
-  }
 });
---
+++ chalkboardTally/src/components/ben.js
@@ -0,0 +1,99 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import moment from 'moment';
+
+class Ben extends Component {
+    state = {
+        startDate: moment("2018-07-18", "YYYY-MM-DD"),
+        totalDays: 0,
+        currentDate: moment(),
+        dayCount: -1,
+        days: [],
+        isTallyComplete: false
+    }
+
+    componentDidMount(){
+    const today = moment(this.state.currentDate);
+    const start = moment(this.state.startDate);
+    const calculatedDays = today.diff(start, 'days');
+    this.setState({ totalDays: calculatedDays});
+    this.interval = setInterval(() => this.tick(), 50);
+    }
+
+    tick() {
+        if(this.state.dayCount + 1 < this.state.totalDays){
+            this.setState(prevState => ({
+            dayCount: prevState.dayCount + 1,
+            days: prevState.days.concat({ key: (prevState.dayCount + 1).toString() })
+            }));  
+        } else {
+            clearInterval(this.interval);
+            this.setState({isTallyComplete: true})
+        }
+    }
+    render() {
+        let summary = this.state.isTallyComplete ? 
+            <View>
+                <Text style={{color: 'white', fontSize: 40, fontFamily: 'ChalkboardSE-Bold'}}>{this.state.totalDays} Days</Text>
+            </View> 
+            : null;
+
+        return (
+            <View style={styles.container}>
+                <Text style={{color: 'white', fontSize: 40, fontFamily: 'ChalkboardSE-Bold'}}>Ben's Tron Tally</Text>
+                <FlatList
+                data={this.state.days}
+                style={styles.list}
+                horizontal={false}
+                numColumns={35}
+                renderItem={({item}) => <Text style={ (parseInt(item.key)+1)%5==0 ? styles.tallyRotate : styles.tally}></Text>}
+                />
+                {summary}
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    summary: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+      fontSize: 16,
+      textAlign: "center",
+    },
+    list: {
+      flexDirection: "column",
+    //   marginTop: 25,
+    },
+    tally: {
+      display: "flex",
+      height: 30,
+      width: 1,
+      borderWidth: 1,
+      borderColor: "lightgrey",
+      borderStyle: "solid",
+      marginRight: 4,
+      marginTop: 5,
+    },
+    tallyRotate: {
+      display: "flex",
+      height: 40,
+      width: 1,
+      borderWidth: 1,
+      borderColor: "lightgrey",
+      borderStyle: "solid",
+      marginRight: 4,
+      marginTop: 0,
+      left: -15,
+      transform: [{rotate: '320deg'}],
+    }
+  });
+
+export default Ben;

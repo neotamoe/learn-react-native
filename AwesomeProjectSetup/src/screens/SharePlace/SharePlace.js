d355b606fe1998999de48eb95ce48d4a2c1826ae
Neota Moe <neota.moe@gmail.com>
2019-02-20 15:16:18 -0600

    styling and adding content placeholders for share place screen

--- AwesomeProjectSetup/src/screens/SharePlace/SharePlace.js
+++ AwesomeProjectSetup/src/screens/SharePlace/SharePlace.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, StyleSheet, ScrollView, Image} from 'react-native';
 import { connect } from 'react-redux';
 import {Navigation} from 'react-native-navigation';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import MainText from '../../components/UI/MainText/MainText';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import imagePlaceholder from '../../assets/walker-art-center.jpg'
 
-import InputAndButton from '../../components/InputAndButton/InputAndButton';
 import {addPlace} from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
@@ -31,12 +34,53 @@ class SharePlaceScreen extends Component {
     
     render() {
         return (
-            <View>
-                <InputAndButton onPlaceAdded={this.placeAddedHandler}/>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <MainText>
+                        <HeadingText>Share a Place With Us!</HeadingText>
+                    </MainText>
+                    <View style={styles.placeholder}>
+                        <Image source={imagePlaceholder} style={styles.imagePreview} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Pick Image" />
+                    </View>
+                    <View style={styles.placeholder}>
+                        <Text>Map</Text>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Locate Me" />
+                    </View>
+                    <DefaultInput placeholder="Place Name"/>
+                    <View style={styles.button}>
+                        <Button title="Share Place!"/>
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    placeholder: { 
+        borderWidth: 1,
+        borderColor: 'black',
+        backgroundColor: '#eee',
+        width: "80%",
+        height: 150
+    },
+    button: {
+        margin: 8
+    },
+    imagePreview: {
+        width: '100%',
+        height: '100%'
+    }
+})
 
 const mapStateToProps = {
 

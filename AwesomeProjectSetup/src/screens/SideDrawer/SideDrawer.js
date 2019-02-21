85e0dc1136aef1ea7b17a4c3735388f3ccc0d47e
Neota Moe <neota.moe@gmail.com>
2019-02-20 18:49:20 -0600

    tweaks to keep more in line with tutorial

--- AwesomeProjectSetup/src/screens/SideDrawer/SideDrawer.js
+++ AwesomeProjectSetup/src/screens/SideDrawer/SideDrawer.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, Button, StyleSheet, SafeAreaView} from 'react-native';
+import {View, Text, Button, StyleSheet, SafeAreaView,TouchableOpacity} from 'react-native';
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 class SideDrawer extends Component {
     render() {
         return (
             <SafeAreaView style={styles.safeArea}>
-            <View style={styles.container}>
-                <Text>SideDrawer</Text>
-                <View style={styles.button}>
-                    <ButtonWithBackground style={styles.button} color="#29aaf4" onPress={() => alert("Logout")}>
-                        Logout
-                    </ButtonWithBackground>
+                <View style={styles.container}>
+                    <TouchableOpacity style={styles.sideDrawerItem}>
+                        <Icon style={styles.icon} name="ios-log-out" size={30} color="#bbb"/>
+                        <Text>Sign Out</Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
             </SafeAreaView>
         );
 
@@ -21,21 +20,23 @@ class SideDrawer extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 22,
-        paddingLeft: 10,
-        backgroundColor: "white",
-        flex: 1
-    },
     safeArea: {
         flex: 1,
         backgroundColor: 'white'
     },
-    button: {
+    container: {
+        paddingTop: 30,
+        backgroundColor: 'white',
         flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        paddingTop: 10,
+    },
+    sideDrawerItem: {
+        flexDirection: "row",
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#eee'
+    },
+    icon: {
+        marginRight: 10
     }
 });
 

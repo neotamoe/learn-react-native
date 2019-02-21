e2cfde7789555434923c7d3e4cfd4068b804f2b3
neotamoe <neota.moe@gmail.com>
2019-02-21 11:24:19 -0600

    changes to buttonWithBackground based on platform

--- AwesomeProjectSetup/src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ AwesomeProjectSetup/src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {TouchableOpacity, Text, View, StyleSheet} from 'react-native';
+import {TouchableOpacity, Text, View, StyleSheet, Platform, TouchableNativeFeedback} from 'react-native';
 
 const buttonWithBackground = props => {
+    const content = (
+        <View style={[styles.button, {backgroundColor: props.color}]}>
+            <Text>{props.children}</Text>
+        </View>
+    );
+    if(Platform.OS === 'android'){
+        return (
+            <TouchableNativeFeedback onPress={props.onPress}>
+                {content}
+            </TouchableNativeFeedback>    
+        );
+    } 
     return (
         <TouchableOpacity onPress={props.onPress}>
-            <View style={[styles.button, {backgroundColor: props.color}]}>
-                <Text>{props.children}</Text>
-            </View>
+            {content}
         </TouchableOpacity>
-    );
+    );    
 };
 
 const styles = StyleSheet.create({

b992d2cbc449f915aa6071dc68a704b8e6adb667
neotamoe <neota.moe@gmail.com>
2019-02-28 11:17:50 -0600

    add thunk, starting refactor to store data in firebase db

--- AwesomeProjectSetup/src/store/actions/places.js
+++ AwesomeProjectSetup/src/store/actions/places.js
@@ -1,12 +1,27 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => { 
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
-    };
+    return dispatch => {
+        const placeData = {
+            name: placeName, 
+            location: location
+        }
+        fetch("https://awesome-places-db.firebaseio.com/places.json", {
+            method: 'POST',
+            body: JSON.stringify(placeData)
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())  // this is needed when using fetch
+        .then(parsedRes => {
+            console.log(parsedRes)
+        })
+    }
+    // return {
+    //     type: ADD_PLACE,
+    //     placeName: placeName,
+    //     location: location,
+    //     image: image
+    // };
 };
 
 export const deletePlace = (key) => { 

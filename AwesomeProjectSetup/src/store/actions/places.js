fb4c49d473761c563751225e04489c7145d4dc0b
neotamoe <neota.moe@gmail.com>
2019-03-01 11:46:36 -0600

    added firebase function, working toward storing image

--- AwesomeProjectSetup/src/store/actions/places.js
+++ AwesomeProjectSetup/src/store/actions/places.js
@@ -2,13 +2,27 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => { 
     return dispatch => {
-        const placeData = {
-            name: placeName, 
-            location: location
-        }
-        fetch("https://awesome-places-db.firebaseio.com/places.json", {
+        console.log('image in addPlace: ', image);  // this is ok
+        // return fetch("https://console.firebase.google.com/project/awesome-places-db/overview",{
+        return fetch("https://us-central1-awesome-places-db.cloudfunctions.net/storeImage",{
             method: 'POST',
-            body: JSON.stringify(placeData)
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+            const placeData = {
+                name: placeName, 
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesome-places-db.firebaseio.com/places.json", {
+                method: 'POST',
+                body: JSON.stringify(placeData)
+            });
         })
         .catch(err => console.log(err))
         .then(res => res.json())  // this is needed when using fetch
@@ -16,12 +30,6 @@ export const addPlace = (placeName, location, image) => {
             console.log(parsedRes)
         })
     }
-    // return {
-    //     type: ADD_PLACE,
-    //     placeName: placeName,
-    //     location: location,
-    //     image: image
-    // };
 };
 
 export const deletePlace = (key) => { 

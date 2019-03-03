e7502c4a1fe4edb8905f4862e2f0943faf57f584
Neota Moe <neota.moe@gmail.com>
2019-03-02 22:20:36 -0600

    initial steps toward getting authentication set up

--- AwesomeProjectSetup/src/store/actions/auth.js
+++ AwesomeProjectSetup/src/store/actions/auth.js
@@ -1,8 +1,30 @@
 import { TRY_AUTH } from './actionTypes';
+import authKey from '../../../authKey';
 
 export const tryAuth = (authData) => { 
-    return {
-        type: TRY_AUTH,
-        authData: authData
-    };
+    return dispatch => {
+        dispatch(authSignup(authData));
+    }
 };
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + authKey, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Authentication Failed!  Please try again.");
+        })
+        .then(res => res.json())
+        .then(parsedRes => console.log(parsedRes))
+    }
+}

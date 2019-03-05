a1d064f05754f68e4992cef0b21538e51d0f8158
Neota Moe <neota.moe@gmail.com>
2019-03-05 14:56:32 -0600

    better error handling and delete images from storage on delete from db

--- AwesomeProjectSetup/functions/index.js
+++ AwesomeProjectSetup/functions/index.js
@@ -54,7 +54,8 @@ exports.storeImage = functions.https.onRequest((request, response) => {
                                 "/o/" + 
                                 encodeURIComponent(file.name) +
                                 "?alt=media&token=" +
-                                uuid
+                                uuid,
+                            imagePath: "/places/" + uuid + ".jpg"
                         });
                     } else {
                         console.log(err);
@@ -69,3 +70,11 @@ exports.storeImage = functions.https.onRequest((request, response) => {
 
     });
 });
+
+exports.deleteImage = functions.database.ref("/places/{placeId}").onDelete(snapshot => {
+    const placeData = snapshot.val();
+    const imagePath = placeData.imagePath;
+    
+    const bucket = gcs.bucket("awesome-places-db.appspot.com");
+    return bucket.file(imagePath).delete();
+})
